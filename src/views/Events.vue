bdaff5c6801d5af1b2d37fc6126aa064c49b6774
Ezra Rabinsky <ezra.rabinsky@gmail.com>
2020-05-06 10:58:46 -0500

    Add accordion layout to events page

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -2,11 +2,15 @@
 <div class="events">
     <h1 class="display-3 hidden-xs-only">Events</h1>
       <h3 class="display-2 hidden-sm-and-up">Events</h3>
+      <event-template></event-template>
   </div>    
 </template>
 <script>
+import EventTemplate from '@/components/EventTemplate'
 export default {
-    
+    components: {
+      'event-template': EventTemplate
+    }
 }
 </script>
 <style scoped>
