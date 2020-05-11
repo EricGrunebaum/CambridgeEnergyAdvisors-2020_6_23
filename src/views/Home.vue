9231111de3bf24d0a6888849ab7440c7abb49731
Ezra Rabinsky <ezra.rabinsky@gmail.com>
2020-05-11 15:27:28 -0500

    Progress on projects relayout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
     <h1 class="display-1 my-4 hidden-sm-and-up">Cambridge Energy Advisors</h1>
     </v-div>
     <v-divider class="my-4 primary"></v-divider>
-      <div v-for="blurb in blurbs" :key="blurb">
+      <div v-for="(blurb, index) in blurbs" :key="blurb">
       <hero-image
       
       :title="blurb.head"
@@ -22,9 +22,9 @@
       :route="cards[index].route"
       ></card-template> -->
       <home-blurb class="ma-12"
-      :title="blurb.head"
-      :body='blurb.body'
-      :route="blurb.route"></home-blurb>
+      :title="cases[index].title"
+      :body='cases[index].body'
+      :route="cases[index].route"></home-blurb>
       </v-row>
     
     </div>
@@ -50,32 +50,30 @@ export default {
   data(){
     return {
       blurbs: [
-                { 
-                  title: `Cambridge Energy Advisors`,               
-                  head: `Business Development ~ Clean Energy &  Resource Efficiency`,
-                  body: `Our approach to business development is grounded in a 20 year foundation of science communications in the documentary and science museum disciplines. Effectively communicating with an economy of words is critical to engaging audiences, whether customers, investors or partners.`,
+                {              
+                  head: `Fundraising and Impact Investment`,
+                  body: `Our approach to investment focuses on providing great value for funds, companies and investors. Impact investment opportunities do not require lowering financial returns to generate positive environmental or social values, indeed multiple studies indicate the reverse to be true. Holding the FINRA Series 82 + 63 licenses, and working with team of partners to source deals, we connect investors to opportunities while sourcing capital for companies and funds focusing on clean energy and resource efficiency.`,
                   source: require(`../../public/trees.jpg`),
                   route: `/about`
                 },
                 {
-                  head: `Fundraising and Impact Investment`,
-                  body: `Our approach to investment focus on providing value for both the recipient and the investor. Impact investment opportunities no longer require reducing financial return to generate positive environmental or social value. With an FINRA Series 82 + 63 licenses, and team of partners sourcing deals, we connect investors to opportunities while sourcing capital for companies and funds focusing on Clean Energy and Resource Efficiency.`,
+                  head: `Project Development – Solar,  Commercial Energy Efficiency & Environmental Justice`,
+                  body: `Our project development work focuses on developing and funding commercial scale solar and energy efficiency upgrades along with pro bono environmental justice work. We work with non-profit and for-profit entities bringing technical expertise, sources of off-balance sheet funding when needed, visioning and community/stakeholder engagement to help advance profitable projects that increase sustainability.`,
                   source: require(`../../public/energy_circle.jpg`),
                   route: `/about`
                 },
               ],
-      cards: [
+      cases: [
               {
-                photo: require('../../public/science.jpg'),
-                head: `Case Study: European Pharma Catalyst/Green Chemistry Firm`,
-                content: `For this EU co., entering the US market through the globally important Kendall Square pharma cluster was critical to gaining market share for its world-leading catalysts that reduce toxic solvents and energy required to manufacture pharmaceuticals.`,
-                route: '/projects'
+                title: `Case Study: Solar Tax Equity and Bond Fund`,
+                body: `For this startup solar commercial fund, we helped expand their investor base, bringing in our network of high net-worth, family office and foundation investors, raising total of $2.25 million. This funding in turn, allowed them to build ~1 megawatt of rooftop solar for a broad spectrum of non-profits, schools, houses of worship, REITs and other businesses while generating great returns for investors.`,
+                route: `/projects`,
               },
               {
-                photo: require('../../public/energy_circle.jpg'),
-                head: `Case Study: Solar Tax Equity and Bond fund`,
-                content: `For this startup solar fund, we helped broaden their investor base and bring in a broad network of high net-worth, family office and foundation investors, raising total of $2.25 million which allowed them to build approximately 1 megawatt of commercial scale solar for a broad spectrum of non-profits, REITs and other businesses.`,
-                route: '/projects'
+                title: `Case Study: Commercial Solar in Harvard Square, Cambridge`,
+                body: `This commercial solar array on a signficant block in Harvard Square has three distinct phases – first is a traditional flat roof solar array, second is a publicly visible solar array on a rooftop mechanical structure and finally, will be translucent panels mounted above a dramatic atrium space, providing both shading properties and power generation.`,
+                route: `/projects`,
+            
               }
              ]
       
