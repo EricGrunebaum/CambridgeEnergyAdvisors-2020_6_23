d215b9fe049bf0ed11e394ed4f9802982b2672d8
Ezra Rabinsky <ezra.rabinsky@gmail.com>
2020-05-19 08:45:29 -0500

    Add Data AOS to 'home' view & add 'business dev' section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
     <v-divider class="my-4 primary"></v-divider>
       <div v-for="(blurb, index) in blurbs" :key="blurb">
       <hero-image
-      
+      data-aos="fade-in" data-aos-duration="1000"
       :title="blurb.head"
       :body="blurb.body"
       :photo="blurb.source"
@@ -22,6 +22,7 @@
       :route="cards[index].route"
       ></card-template> -->
       <home-blurb class="ma-12"
+      data-aos="fade-in" data-aos-duration="500" data-aos-easing="ease-in-sine"
       :title="cases[index].title"
       :body='cases[index].body'
       :route="cases[index].route"></home-blurb>
@@ -62,6 +63,12 @@ export default {
                   source: require(`../../public/energy_circle.jpg`),
                   route: `/about`
                 },
+                {
+                  head: `business development ~ clean energy &amp; resource efficiency`,
+                  body: `Our approach to business development is grounded in a 20 year foundation of science communications for documentary films and science museums. Communicating your mission powerfully with an economy of words is critical to engaging audiences, whether investors, partners or customers.`,
+                  source: require(`../../public/boston.jpg`),
+                  route: `/about`
+                },
               ],
       cases: [
               {
@@ -74,7 +81,13 @@ export default {
                 body: `This commercial solar array on a signficant block in Harvard Square has three distinct phases – first is a traditional flat roof solar array, second is a publicly visible solar array on a rooftop mechanical structure and finally, will be translucent panels mounted above a dramatic atrium space, providing both shading properties and power generation.`,
                 route: `/projects`,
             
-              }
+              },
+              {
+                title: `project: european pharma catalyst/green chemistry firm`,
+                body: `For this EU co., entering the US market through the globally critical Kendall Square pharma cluster was the key to gaining market share for its world-leading catalysts that reduce toxic solvents and the energy required to manufacture pharmaceuticals.`,
+                route: `/projects`,
+            
+              },
              ]
       
     }
