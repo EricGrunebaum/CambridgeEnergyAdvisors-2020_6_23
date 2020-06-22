e2572014f3dd2566fa4bf8dc39de6c3533db3016
Ezra Rabinsky <ezra.rabinsky@gmail.com>
2020-06-22 05:53:36 -0500

    Refactor v-toolbar to v-app-bar for vuetify support

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
    <nav>
        <v-container>
-            <v-toolbar flat app prominent> 
+            <v-app-bar flat app prominent light absolute="" style="background-color: white; margin: 2rem"> 
                 <v-app-bar-nav-icon class="hidden-md-and-up primary--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
                 <v-toolbar-title class="text-uppercase hidden-xs-only text-start">
-                    <div><a href="/"><v-img class="" :src="logo" max-height="120"></v-img></a></div>
+                    <div id="logo"><a href="/"><v-img class="" :src="logo" max-height="120"></v-img></a></div>
                     <!-- <a href="/"><div class="logo"><span class="primary--text text-lowercase">cambridge energy advisors.</span></div></a> -->
                 </v-toolbar-title>
                 <v-toolbar-title class="text-uppercase hidden-sm-and-up text-start">
@@ -25,7 +25,7 @@
                        
                     </v-tabs>
                 </div>
-            </v-toolbar>
+            </v-app-bar>
         </v-container>
             <v-navigation-drawer app  v-model="drawer" nav>
                  <v-list-item>
@@ -78,6 +78,9 @@ export default {
 </script>
 <style scoped>
     
+    #logo{
+        margin-left: 2rem;
+    }
     .logo {
         background-image: url('../../public/CEA1-2.png');
         min-height: 120px;
