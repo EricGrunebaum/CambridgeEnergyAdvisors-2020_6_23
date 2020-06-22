77964d72de6661c40226a9a7f046edfaf8f19892
Ezra Rabinsky <ezra.rabinsky@gmail.com>
2020-06-22 05:54:03 -0500

    Refactor v-title to h3 and h5 for vuetify support

--- src/components/HomeBlurb.vue
+++ src/components/HomeBlurb.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="ma-4">
-        <v-title class="primary--text display-1 hidden-xs-only content-title">{{ title }}</v-title>
-        <v-title class="primary--text hidden-sm-and-up content-title">{{ title }}</v-title>
+        <h3 class="primary--text display-1 hidden-xs-only content-title">{{ title }}</h3>
+        <h5 class="primary--text hidden-sm-and-up content-title">{{ title }}</h5>
         <p class="text-justify ma-4 hidden-xs-only content-body">{{ body }}</p>
         <p class="text-center my-4 hidden-sm-and-up content-body">{{ body }}</p>
         <v-btn class="" tile="" color="primary" outlined="" hover="true" :to="route">Read More<v-icon right>mdi-chevron-right</v-icon></v-btn>
